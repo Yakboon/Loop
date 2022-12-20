88cbbdacf3acb49ae119787efb2d880d0c2f40cc
Yakboon <120273912+Yakboon@users.noreply.github.com>
2022-12-20 18:00:45 +0300

    Loops
    It is just for fun.

---
+++ Testofloop/src/Quiz.java
@@ -0,0 +1,13 @@
+
+public class Quiz {
+
+	public static void main(String[] args) {
+		System.out.println("+---------+");
+		for (int i = 1; i <= 3; i++) {
+			System.out.println("\\   /");
+			System.out.println("/   \\");
+		}
+		System.out.println("+---------+");
+	}
+
+}
